b428191ea07a0365d580f1399ac7806388d8a76e
tymoteusz.wachal <tymoteusz.wachal@currency-one.com>
2022-06-22 14:42:09 +0200

    add form

--- app/src/main/java/com/example/androidzaliczenie/AddPlantsActivity.kt
+++ app/src/main/java/com/example/androidzaliczenie/AddPlantsActivity.kt
@@ -2,10 +2,17 @@ package com.example.androidzaliczenie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AddPlantsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_plants)
+
+        val safeButton = findViewById<Button>(R.id.button2)
+
+        safeButton.setOnClickListener{
+            val name = findViewById<Button>(R.id.button2)
+        }
     }
 }
